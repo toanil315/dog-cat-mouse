85b4774e0e44f6fb619b01e0ba2f7f878ff6fd61
toan315 <congtoan.dn@gmail.com>
2020-03-25 22:45:06 +0700

    add eat method

--- cat.js
+++ cat.js
@@ -1,5 +1,9 @@
 function Cat(name, color) {
     this.name = name;
     this.color = color
+    this.stomach = [];
+}
+Cat.prototype.eat = function(mouse) {
+    this.stomach.push(mouse);
 }
 module.exports = Cat;
